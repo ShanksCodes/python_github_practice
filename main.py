ea5bbde1e92bf228322dbb8b3001dee642be8c11
shanks <abhishektyagiofficial99@gmail.com>
2026-04-23 18:22:43 +0530

    track test

--- main.py
+++ main.py
@@ -3,9 +3,5 @@ def print_hi(name):
 
 
 if __name__ == '__main__':
-<<<<<<< HEAD
-    print_hi('Hello from Branch for rebase fix hash test')
-=======
-    print_hi('Hello from hash test')
->>>>>>> 50bf40c (hash test commit)
+    print_hi('Hello track test')
 
