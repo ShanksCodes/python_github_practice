88eddb621f486a06ccb1c501d26184296fecd426
shanks <abhishektyagiofficial99@gmail.com>
2026-04-26 13:08:52 +0530

    B1 add feature 1

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ def print_hi(name):
 def main_feature_1():
     print("Main feature 1")
 
-if __name__ == '__main__':
-    print_hi('Hello from MAIN new')
+def branch_feature_1():
+    print("Branch feature 1")
 
+
+if __name__ == '__main__':
+    print_hi('Hello from branch B1')
